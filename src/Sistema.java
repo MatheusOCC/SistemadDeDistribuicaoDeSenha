8036aada266ddc3649111e5e42985d6c492aa7a0
Matheus Oliver de Carvalho Cerqueira <escrevaparamatheus@gmail.com>
2018-07-12 18:07:35 -0300

    Add files via upload

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -18,10 +20,11 @@ public class Sistema {
 		return sSenha;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 	
 		String prioridade;
-		prioridade = "P";
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		prioridade = in.readLine();
 		
 		int numero;
 		numero = 1;
@@ -55,7 +58,7 @@ public class Sistema {
 			filaNormal.poll();
 		}
 		
-		//System.out.println("Sua senha (Aritmética): "+senha1.getPrioridade()+senha1.getNumero());
+		//System.out.println("Sua senha (Aritmï¿½tica): "+senha1.getPrioridade()+senha1.getNumero());
 
 	}
 
